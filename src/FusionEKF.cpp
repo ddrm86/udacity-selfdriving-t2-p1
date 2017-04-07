8b729ea6a6815f3efd85db428d1886b16e6fb223
David del Río Medina <ddrm86@gmail.com>
2017-04-07 09:56:03 +0200

    Assign proper R and H matrices before calling update functions

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -139,8 +139,12 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    */
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+    ekf_.R_ = R_radar;
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
     ekf_.Update(measurement_pack.raw_measurements_);
   } else {
+    ekf_.R_ = R_laser_;
+    ekf_.H_ = H_laser_
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   }
 
